46df2f885d44da65b7b8fe31a53ebc243b44d32a
DougDev <douglas.ofc.2004@gmail.com>
2025-05-04 05:37:37 +0000

    Faz a conexão de algumas telas do front com o Back e cria a estrutura inicial do cardápio

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,20 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    return '👋 Olá, Flask está rodando!'
+    return render_template('index.html')
 
-@main_bp.route('/echo', methods=['POST'])
-def echo():
-    data = request.get_json() or {}
-    return jsonify({'você_enviou': data})
+@main_bp.route('/login')
+def login():
+    return render_template('login.html')
+
+@main_bp.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@main_bp.route('/login/registrar-cardapio')
+def registrar_cardapio():
+    return render_template('registrar.cardapio.html')
+
+@main_bp.route('/login/cardapio')
+def cardapio():
+    return render_template('cardapio.html')
